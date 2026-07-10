5139e6c63813901a7c2344808f0b4579ed7c21a2
jaymane862 <88193679+jaymane862@users.noreply.github.com>
2026-07-10 17:29:03 +0530

    Update TestNGTodo1.java

--- src/test/java/com/lambdatest/TestNGTodo1.java
+++ src/test/java/com/lambdatest/TestNGTodo1.java
@@ -21,7 +21,7 @@ public class TestNGTodo1 {
 
     @BeforeMethod
     public void setup(Method m, ITestContext ctx) throws MalformedURLException {
-        System.setProperty("webdriver.chrome.driver", "/Users/pramod/Documents/driver/chromedriver");
+        
         driver = new ChromeDriver();
 
     }
